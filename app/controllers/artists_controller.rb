8af71f9c5bbff4e05dba5e5ef6e013abbdedc5d7
aslanblack <aslanblack@gmx.com>
2018-09-02 14:17:17 +0000

    initial commit

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,21 @@
+class ArtistsController < ApplicationController
+    before_action :auth
+    
+    def index 
+        if !params[:artist_name].blank?
+            @artists = RSpotify::Artist.search(params[:artist_name])
+            
+        end
+    end
+    
+    def auth
+      
+      RSpotify.authenticate("a297e69c0afd4289983ecdf5e98f9d8e", "4d8b42920b41431d82d539ea80789664")
+      #response = HTTParty.get('https://api.spotify.com/v1/search?q=artist:'+@name.to_s+'&type=artist', headers={secret:"4d8b42920b41431d82d539ea80789664"})
+      #RSpotify::Artist.search('bob')
+    end
+    
+    
+
+   
+end
